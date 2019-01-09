313236deaddffd786e1f9c9b5bdee11a17f587af
Phsouza159 <pauloh159@live.com>
2019-01-09 03:19:29 -0200

    NFCation v 1.0.0 :: refatoramento (class com ponteiro)

--- testes/teste.php
+++ testes/teste.php
@@ -6,13 +6,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Notification;
 
+/**
+ * Class Teste
+ * @package teste
+ */
 class Teste extends Notification\NFCation {
     public $a;
     public $b;
     public $c;
     public $cpf = "064.111.661-64";
     public $cnpj = "78.132.679/0001-61";
-
 
     public function __construct(){
         $this->a = 10;
@@ -23,14 +26,31 @@ class Teste extends Notification\NFCation {
         parent::GetVar()
             ->IsNumeric($this->a)
             ->IsNumeric($this->b)
-            ->IsNull($this->c)
+            ->IsNotNull($this->c)
             ->IsCpf($this->cpf)
             ->IsCnpj($this->cnpj);
     }
 }
 
+class Teste2 extends Notification\NFCation
+{
+
+    /**
+     * Teste2 constructor.
+     */
+    public function __construct()
+    {
+    }
+}
+
+
 $a = new Teste();
+$b = new Teste2();
+
+$b->AddNotificationObject( $a );
 
 echo "<pre>";
 
 print_r($a);
+
+print_r($b);
